ddbbc57e797a08ee5a4d6f7ce25e9acf3e0efd36
Alvaro de sa <9alvaro.sa4@gmail.com>
2024-10-22 22:43:07 -0300

    Implementaçoes de tela

---
+++ app/internas/_layout.tsx
@@ -0,0 +1,29 @@
+import { Tabs } from "expo-router";
+
+export default function Layout() {
+  return (
+    
+    <Tabs 
+      screenOptions={{
+        headerStyle: {backgroundColor: "#F60"},
+        tabBarStyle: {backgroundColor: "070A52"},
+        headerTitleAlign: 'center',
+        headerTintrColor: '#FFF'
+       
+      }}
+    >
+      <Tabs.Screen name="tasks" options={{ 
+        headerTitle: "Tarefas"
+      }}/>
+      
+      <Tabs.Screen name="user" options={{ 
+        headerTitle: "Dados do Usuário"
+        }} />
+
+    <Tabs.Screen name="about" options={{
+        headerTitle: "Sobre o aplicativo"
+        
+     }}/>
+    </Tabs>
+  );
+}
